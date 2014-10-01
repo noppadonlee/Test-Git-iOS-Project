731be069940def284fb8cf67aa0cadf655bf09ba
Noppadon Lee <noppadon.lee@gmail.com>
2014-10-01 17:37:10 +0700

    added a test class

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Noppadon Lee on 10/1/2557 BE.
+//  Copyright (c) 2557 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
